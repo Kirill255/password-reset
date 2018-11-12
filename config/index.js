38b17ac80ea777b3735c43b0a6b4289819683f12
Kirill255 <jhgbnm1@mail.ru>
2018-11-12 17:52:01 +0300

    initial

---
+++ config/index.js
@@ -0,0 +1,8 @@
+require('dotenv').config()
+
+module.exports = {
+    MAIL_HOST: process.env.MAIL_HOST,
+    MAIL_PORT: process.env.MAIL_PORT,
+    MAIL_USERNAME: process.env.MAIL_USERNAME,
+    MAIL_PASSWORD: process.env.MAIL_PASSWORD
+}
